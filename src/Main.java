da933f769f799f2de3eca447fe0604bcc6f088f2
Ritika <rkjane333@gmail.com>
2024-04-16 23:40:17 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+        System.out.println("The sum of these Numbers is: ");
+
+        int num1 = 4;
+        int num2 = 5;
+        int num3 = 9;
+        int num4 = 8;
+
+        int sum = num1 + num2 + num3 + num4;
+        System.out.println(sum);
+    }
+}
